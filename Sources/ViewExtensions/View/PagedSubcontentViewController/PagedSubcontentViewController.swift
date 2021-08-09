d3b5cf5481f41c11fc01698eb7e711c02b326857
Ilya Senchukov <senchukov02@gmail.com>
2021-08-10 01:49:39 +0300

    feat: replaced PageViewController with UICollectionView

--- Sources/ViewExtensions/View/PagedSubcontentViewController/PagedSubcontentViewController.swift
+++ Sources/ViewExtensions/View/PagedSubcontentViewController/PagedSubcontentViewController.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  File
+//  PagedSubcontentViewController.swift
+//  PagedSubcontentViewController
 //
 //  Created by Ilya Senchukov on 07.08.2021.
 //
@@ -14,12 +14,18 @@ open class PagedSubcontentViewController: UIViewController {
 
     @IBOutlet weak var topContentHeightConstraint: NSLayoutConstraint!
     @IBOutlet public weak var topContentContainerView: UIView!
-    @IBOutlet public weak var mainContentContainerView: UIView!
+    @IBOutlet public weak var collectionView: UICollectionView!
 
     public var topContent: UIViewController!
+    public let pages = PublishRelay<[UIViewController]>()
 
+    private var _disposeBag: DisposeBag?
     private var topContentContainer: UIViewController!
-    private var pageViewController: UIPageViewController = .init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    private lazy var dataSource = RxCollectionViewSectionedAnimatedDataSource<PagedSection>(
+        configureCell: { _, collectionView, indexPath, item in
+            collectionView.dequeueContainerCell(with: item, for: indexPath)
+        }
+    )
 
     public init() {
         super.init(nibName: PagedSubcontentViewController.reuseIdentifier, bundle: .module)
@@ -37,33 +43,30 @@ open class PagedSubcontentViewController: UIViewController {
         }
     }
 
-    public override func viewDidLoad() {
+    open override func viewDidLoad() {
         super.viewDidLoad()
-        pageViewController.dataSource = self
-        setupView()
+        _setupView()
+        _setupBindings()
     }
 
     open func getTopContent() -> UIViewController {
         fatalError()
     }
 
-}
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
-extension PagedSubcontentViewController {
-
-    public func setViewController(
-        _ vc: UIViewController,
-        direction: UIPageViewController.NavigationDirection = .forward,
-        animated: Bool = true
-    ) {
-        pageViewController.setViewControllers([vc], direction: direction, animated: animated, completion: nil)
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = collectionView.frame.size
+        }
     }
 
 }
 
 private extension PagedSubcontentViewController {
 
-    func setupView() {
+    func _setupView() {
+        view.backgroundColor = .red
 
         topContentContainer = SelfSizingViewController()
         topContentContainer.move(to: self, viewPath: \.topContentContainerView)
@@ -73,26 +76,42 @@ private extension PagedSubcontentViewController {
         topContent.move(to: topContentContainer)
         topContent.view.fit(into: topContentContainer.view)
 
-        pageViewController.move(to: self, viewPath: \.mainContentContainerView)
-        pageViewController.view.fit(into: mainContentContainerView)
-        
-        mainContentContainerView.layer.cornerRadius = 26
-        mainContentContainerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        mainContentContainerView.layer.masksToBounds = true
+        collectionView.registerCell(ContainerCell.self)
+        collectionView.layer.cornerRadius = 26
+        collectionView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        collectionView.layer.masksToBounds = true
 
         view.forceLayout()
     }
 
-}
+    func _setupBindings() {
 
-extension PagedSubcontentViewController: UIPageViewControllerDataSource {
+        _disposeBag = DisposeBag {
 
-    open func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        fatalError("viewControllerBefore is not implemented")
-    }
+            pages
+                .map { [PagedSection.init(items: $0)] }
+                .bind(to: collectionView.rx.items(dataSource: dataSource))
 
-    open func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        fatalError("viewControllerAfter is not implemented")
+            collectionView.rx
+                .didScroll
+            .observe(on: MainScheduler.asyncInstance)
+            .subscribe(with: self, onNext: { owner, _ in
+
+                let offset = owner.collectionView.contentOffset.x
+
+                if offset < 0 {
+                    var newOffset = owner.collectionView.contentOffset
+                    newOffset.x = 0
+                    owner.collectionView.setContentOffset(newOffset, animated: false)
+                } else if offset > owner.collectionView.contentSize.width {
+                    var newOffset = owner.collectionView.contentOffset
+                    newOffset.x = owner.collectionView.contentSize.width
+                    owner.collectionView.setContentOffset(newOffset, animated: false)
+                }
+            })
+
+        }
+
     }
 
 }
