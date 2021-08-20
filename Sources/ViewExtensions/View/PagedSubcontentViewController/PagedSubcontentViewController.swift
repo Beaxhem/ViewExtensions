8fb5a5181593db569e19264a4a82ce02474bd75c
Ilya Senchukov <senchukov02@gmail.com>
2021-08-20 23:17:38 +0300

    feat: looks like page view is developed

--- Sources/ViewExtensions/View/PagedSubcontentViewController/PagedSubcontentViewController.swift
+++ Sources/ViewExtensions/View/PagedSubcontentViewController/PagedSubcontentViewController.swift
@@ -10,17 +10,49 @@ import RxDataSources
 import RxCocoa
 import RxSwift
 
+extension ObservableType {
+
+    func withPrevious() -> Observable<(Element?, Element)> {
+        scan([]) { Array($0 + [$1]).suffix(2) }
+            .map { ($0.count > 1 ? $0.first : nil, $0.last!) }
+    }
+}
+
 open class PagedSubcontentViewController: UIViewController {
 
     @IBOutlet weak var topContentHeightConstraint: NSLayoutConstraint!
     @IBOutlet public weak var topContentContainerView: UIView!
-    @IBOutlet weak var pagedView: HorizontalPagedView!
+    @IBOutlet weak var mainContentContainerView: UIView!
 
     public var topContent: UIViewController!
-    public let pages = PublishRelay<[UIViewController]>()
+    public lazy var pageViewController: UIPageViewController = {
+        let vc = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        vc.delegate = self
+        vc.dataSource = self
+        return vc
+    }()
 
     private var _disposeBag: DisposeBag?
     private var topContentContainer: UIViewController!
+
+    private let touchBegan = PublishRelay<Void>()
+    private let transitionToRelay = PublishRelay<[UIViewController]>()
+
+    private lazy var propertyAnimator: UIViewPropertyAnimator = {
+        .init(duration: 0.5, curve: .easeInOut)
+    }()
+    private var scrollView: UIScrollView {
+        pageViewController.view.subviews.first as! UIScrollView
+    }
+    private var translationX: CGFloat {
+        let translation = scrollView.panGestureRecognizer.translation(in: mainContentContainerView).x
+
+        return translation < 0 ? -translation : translation
+    }
+    var scrollFraction: CGFloat {
+        translationX / mainContentContainerView.frame.width
+    }
+
     private lazy var dataSource = RxCollectionViewSectionedAnimatedDataSource<PagedSection>(
         animationConfiguration: .init(insertAnimation: .none, reloadAnimation: .none, deleteAnimation: .none),
         configureCell: { _, collectionView, indexPath, item in
@@ -41,7 +73,10 @@ open class PagedSubcontentViewController: UIViewController {
 
         guard let container = container as? SelfSizingViewController else { return }
         topContentHeightConstraint?.constant = container.preferredContentSize.height
-        pagedView.didChangeSize()
+
+        pageViewController.viewControllers?.forEach { viewController in
+            viewController.view.frame.size.height = mainContentContainerView.frame.height
+        }
     }
 
     open override func viewDidLoad() {
@@ -54,18 +89,18 @@ open class PagedSubcontentViewController: UIViewController {
         fatalError()
     }
 
-    open override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        pagedView.start()
-    }
-
 }
 
 private extension PagedSubcontentViewController {
 
     func _setupView() {
-        pagedView.parent = self
+        setupTopContent()
+        setupMainContent()
 
+        view.forceLayout()
+    }
+
+    func setupTopContent() {
         topContentContainer = SelfSizingViewController()
         topContentContainer.move(to: self, viewPath: \.topContentContainerView)
         topContentContainer.view.fit(into: topContentContainerView)
@@ -73,23 +108,144 @@ private extension PagedSubcontentViewController {
         topContent = getTopContent()
         topContent.move(to: topContentContainer)
         topContent.view.fit(into: topContentContainer.view)
+    }
 
-        pagedView.layer.cornerRadius = 26
-        pagedView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        pagedView.layer.masksToBounds = true
-        pagedView.isScrollEnabled = false
+    func setupMainContent() {
+        mainContentContainerView.layer.cornerRadius = 26
+        mainContentContainerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        mainContentContainerView.layer.masksToBounds = true
 
-        view.forceLayout()
+        pageViewController.move(to: self)
+        mainContentContainerView.addSubview(pageViewController.view)
+        pageViewController.view.fit(into: mainContentContainerView)
+    }
+
+}
+
+extension UIViewAnimatingState: CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+            case .stopped:
+                return "stopped"
+            case .inactive:
+                return "inactive"
+            case .active:
+                return "active"
+        }
+    }
+
+}
+
+extension UIGestureRecognizer.State: CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+            case .possible:
+                return "possible"
+            case .failed:
+                return "failed"
+            case .cancelled:
+                return "cancelled"
+            case .began:
+                return "began"
+            case .changed:
+                return "changed"
+            case .ended:
+                return "ended"
+        }
+    }
+
+}
+
+extension UIViewAnimatingPosition: CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+            case .current:
+                return "current"
+            case .end:
+                return "end"
+            case .start:
+                return "start"
+        }
+    }
+
+}
+
+private extension PagedSubcontentViewController {
+
+    @objc func test(gesture: UIPanGestureRecognizer) {
+        guard gesture.state == .began else { return }
+        touchBegan.accept(())
+
     }
 
     func _setupBindings() {
+        scrollView.panGestureRecognizer.delaysTouchesBegan = true
+        scrollView.panGestureRecognizer.addTarget(self, action: #selector(test))
 
         _disposeBag = DisposeBag {
 
-            pages
-                .compactMap { $0 }
-                .subscribe(with: self, onNext: { owner, pages in
-                    owner.pagedView.addPages(pages)
+            touchBegan
+                .subscribe(with: self, onNext: { owner, _ in
+                    print("touch began", owner.propertyAnimator.state)
+                    if owner.propertyAnimator.fractionComplete != 0 || owner.propertyAnimator.state == .active {
+                        print("fraction != 0")
+//                        owner.propertyAnimator.stopAnimation(false)
+//                        owner.propertyAnimator.finishAnimation(at: .end)
+                        owner.scrollView.panGestureRecognizer.isEnabled = false
+                        owner.scrollView.panGestureRecognizer.isEnabled = true
+                        owner.propertyAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+                    }
+                })
+
+            transitionToRelay
+                .compactMap { $0.first }
+                .subscribe(with: self, onNext: { owner, controller in
+                    let backgroundColor: UIColor = controller.view.backgroundColor ?? .systemBackground
+                    owner.propertyAnimator.addAnimations {
+                        owner.view.backgroundColor = backgroundColor
+                    }
+                })
+
+            scrollView.rx
+                .didScroll
+                .withLatestFrom(transitionToRelay.compactMap { $0.first })
+                .observe(on: MainScheduler.asyncInstance)
+                .subscribe(with: self, onNext: { owner, controller in
+                    print("scroll")
+                    if owner.propertyAnimator.state == .active &&
+                        controller == owner.pageViewController.viewControllers!.first! {
+                        print("disable if", owner.scrollView.panGestureRecognizer.state)
+                        owner.scrollView.panGestureRecognizer.isEnabled = false
+                        owner.scrollView.panGestureRecognizer.isEnabled = true
+                        owner.propertyAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+                    } else if owner.scrollView.panGestureRecognizer.state == .changed {
+                        print("changed")
+                        owner.propertyAnimator.fractionComplete = owner.scrollFraction
+                    } else {
+
+                        print("disable else", owner.scrollView.panGestureRecognizer.state)
+//                        owner.scrollView.panGestureRecognizer.isEnabled = false
+//                        owner.scrollView.panGestureRecognizer.isEnabled = true
+//                        owner.propertyAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+                    }
+
+                })
+
+            scrollView.rx.willEndDragging
+                .withLatestFrom(transitionToRelay.compactMap { $0.first })
+                .subscribe(with: self, onNext: { owner, latest in
+                    var velocity = owner.scrollView.panGestureRecognizer.velocity(in: owner.mainContentContainerView).x
+                    if velocity < 0 { velocity *= -1 }
+
+                    if owner.scrollView.panGestureRecognizer.isEnabled {
+                        print("will end", !(velocity > 300 || owner.translationX >= owner.scrollView.frame.width / 2))
+                        owner.propertyAnimator.isReversed = !(velocity > 300 || owner.translationX >= owner.scrollView.frame.width / 2)
+                        owner.propertyAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+                    }
+
                 })
 
         }
@@ -97,3 +253,24 @@ private extension PagedSubcontentViewController {
     }
 
 }
+
+extension PagedSubcontentViewController: UIPageViewControllerDataSource {
+
+    open func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        fatalError("Not implemented")
+    }
+
+    open func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        fatalError("Not implemented")
+    }
+
+}
+
+extension PagedSubcontentViewController: UIPageViewControllerDelegate {
+
+    public func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        transitionToRelay.accept(pendingViewControllers)
+    }
+
+}
+
