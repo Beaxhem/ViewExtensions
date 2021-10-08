0c12531025217c89e106d2f6fd34a6f1cfb95fe0
Ilya Senchukov <senchukov02@gmail.com>
2021-10-08 15:04:50 +0300

    feat: add little documentation

--- Sources/ViewExtensions/Extensions/NSLayoutConstraint.swift
+++ Sources/ViewExtensions/Extensions/NSLayoutConstraint.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
+// MARK: Equal
+
 infix operator =>: MultiplicationPrecedence
-public func =><T>(lhs: NSLayoutAnchor<T>, rhs: NSLayoutAnchor<T>) -> NSLayoutConstraint {
+public func => <T>(lhs: NSLayoutAnchor<T>, rhs: NSLayoutAnchor<T>) -> NSLayoutConstraint {
     lhs.constraint(equalTo: rhs)
 }
+
+// MARK: Less than or equal
 
 infix operator <=: MultiplicationPrecedence
 public func <= <T>(lhs: NSLayoutAnchor<T>, rhs: NSLayoutAnchor<T>) -> NSLayoutConstraint {
@@ -21,6 +25,8 @@ public func <=(lhs: NSLayoutDimension, rhs: CGFloat) -> NSLayoutConstraint {
     lhs.constraint(lessThanOrEqualToConstant: rhs)
 }
 
+// MARK: Greater than or equal
+
 infix operator >=: MultiplicationPrecedence
 public func >= <T>(lhs: NSLayoutAnchor<T>, rhs: NSLayoutAnchor<T>) -> NSLayoutConstraint {
     lhs.constraint(greaterThanOrEqualTo: rhs)
@@ -29,6 +35,8 @@ public func >= <T>(lhs: NSLayoutAnchor<T>, rhs: NSLayoutAnchor<T>) -> NSLayoutCo
 public func >=(lhs: NSLayoutDimension, rhs: CGFloat) -> NSLayoutConstraint {
     lhs.constraint(greaterThanOrEqualToConstant: rhs)
 }
+
+// MARK: Constants
 
 public func +(lhs: NSLayoutConstraint, rhs: CGFloat) -> NSLayoutConstraint {
     lhs.constant = rhs
